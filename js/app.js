70c0c198808e7cf23d7d65c0cf0cda7cd35be687
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-25 16:11:02 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// #### Refresher
+
+// So far we have seen:
+
+//     * Strings
+//     * Numbers
+//     * Booleans
+//     * Arrays
+//     * Objects
+
+// When these datatypes are combined, we get a ** data structure **, for example, an array that contains objects that each contain booleans.
+
+// Suppose you are tasked with creating some software, and it is up to ** you ** to determine which datatypes and what data structure to use.For each of the following, write which ** data types ** you would use to represent the data, and then give a small example of the ** data structure **:
+
+//* A light switch that can be either on or off.
+    // booleans
+//* A user's email address.
+    // strings
+//* A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    // object
+//* A list of student names from our class.
+    // array
+//* A list of student names from our class, each with a location.
+    // object
+//* A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    // object
