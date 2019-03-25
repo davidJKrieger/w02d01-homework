88038d4e0f88bc99c998fc5f75addbaedb9d3305
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-25 16:41:04 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -90,3 +90,31 @@ const crazyObject = {
 
 
 // &#x1F534; ** Commit:** "crazyObject"
+
+// ### 4. Object - ception
+
+// With the following object:
+
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+
+//Change the value of`limbo` to`null`.
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
