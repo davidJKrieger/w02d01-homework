ca73281c7540d1e0da9a6bdaca7f641abb9d1400
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-25 16:24:00 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -24,3 +24,20 @@
     // object
 //* A list of student names from our class, each with a location and each with a list of favorite tv shows.
     // object
+
+// 2. Take it Easy
+
+// 1. Make an array that holds all of the colors of the rainbow.
+    const rainbow = ["red", "orange","yellow","green","blue","violet"];
+// 2. Write code that will access "blue"
+    rainbow[4];
+// 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+    const dave = {
+    favoriteFood: "Pizza",
+    hobby: "Hiking",
+    currentTown:"Denver",
+    favoriteDataType: "objects",
+    }
+// 4. Write code that will access your hobby from the object that you just created.
+    dave.hobby
+//     -- -
